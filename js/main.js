e48b5a9487bf72c78b4fb9bb285184c31d9c83f8
seokhj0311 <seokhj0311@naver.com>
2025-11-11 10:15:15 +0900

    20.11.11 작업본

--- js/main.js
+++ js/main.js
@@ -32,10 +32,76 @@ $(document).ready(function(){
     });
     // sec_4 event end
 
+    var num_1 = 'on';
+    var num_2 = 'on';
+    var num_3 = 'on';
+
+    $('.con_6_1').click(function(){
+        if(num_1 == 'on'){
+
+            $('.plus_6_1').css({
+                transform: "rotate(45deg)",
+                transition: "0.5s",
+                color: "#00953B"
+            });
+            num_1 = 'off';
+        }else if(num_1 = 'off'){
+
+            $('.plus_6_1').css({
+                transform: "rotate(0)",
+                transition: "0.5s",
+                color: "unset"
+            });
+            num_1 = 'on';
+        }
+    });
+
+    $('.con_6_2').click(function(){
+        if(num_1 == 'on'){
+
+            $('.plus_6_2').css({
+                transform: "rotate(45deg)",
+                transition: "0.5s",
+                color: "#00953B"
+            });
+            num_1 = 'off';
+        }else if(num_1 = 'off'){
+
+            $('.plus_6_2').css({
+                transform: "rotate(0)",
+                transition: "0.5s",
+                color: "unset"
+            });
+            num_1 = 'on';
+        }
+    });
+
+    $('.con_6_3').click(function(){
+        if(num_1 == 'on'){
+
+            $('.plus_6_3').css({
+                transform: "rotate(45deg)",
+                transition: "0.5s",
+                color: "#00953B"
+            });
+            num_1 = 'off';
+        }else if(num_1 = 'off'){
+
+            $('.plus_6_3').css({
+                transform: "rotate(0)",
+                transition: "0.5s",
+                color: "unset"
+            });
+            num_1 = 'on';
+        }
+    });
+    // sec_6 event end
+
     $(".top_btn_wrap").click(function (){
         $("body, html").stop().animate({
             scrollTop: 0
         }, 1000);
     });
+    // footer event end
 
 });
